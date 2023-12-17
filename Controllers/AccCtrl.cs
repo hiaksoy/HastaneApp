42cf379a9e42330cd4be437085e925679265f008
hiaksoy <64258333+chllnger@users.noreply.github.com>
2023-12-17 20:16:49 +0300

    Initial commit

---
+++ Controllers/AccCtrl.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneApp.Controllers
+{
+    public class AccCtrl : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
