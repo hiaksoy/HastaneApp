9015e4b164b36fb1c06fb024fb8a4072fe268434
hiaksoy <64258333+chllnger@users.noreply.github.com>
2023-12-24 23:06:27 +0300

    --

--- Models/LoginWM.cs
+++ Models/LoginWM.cs
@@ -9,7 +9,7 @@ namespace HastaneApp.Models
         [StringLength(50, ErrorMessage ="Kullanıcı adı en fazla 50 karakter olabilir")]
         public string kullaniciAdi { get; set; }
 
-        [Required(ErrorMessage ="Şifre giriniz")]
+        [Required(ErrorMessage ="Şifre  giriniz")]
         [MinLength(8,ErrorMessage ="Şifre 8 karakterden az olamaz")]
         [MaxLength(20,ErrorMessage ="Şifre 20 Karakterden fazla olamaz")]
         public string sifre { get; set; }
